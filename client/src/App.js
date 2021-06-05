71dcc5aa6088367240087371b2a34baa0805bf74
Rikio Campbell <rikiocamp@gmail.com>
2021-06-05 12:35:00 -0700

    update for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Books from "./pages/Books";
+import Nav from "./components/Nav";
+import Search from './pages/Search'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Wrapper from './components/Wrapper'
+import Saved from './pages/Saved'
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path='/' component={Search} />
+          <Route exact path='/search' component={Search} />
+          <Route exact path='/saved' component={Saved} />
+        </Wrapper>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
-}
+      
+    </Router>
 
+      
+  )
+}
 
 export default App;
