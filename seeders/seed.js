640565be74f8f14e2ae91c12e0914543a63679ba
Rikio Campbell <rikiocamp@gmail.com>
2021-06-05 16:09:15 -0700

    bring back seeds files

---
+++ seeders/seed.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const db = require('../models')
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    'mongodb://localhost/reactbooks'
+)
+
+const bookSeed = [
+    {
+        title: 'test',
+        author: ['test'],
+        image: 'test',
+        link: 'test',
+        description: 'test'
+    },
+    {
+        title: 'test2',
+        author: ['test2'],
+        image: 'test2',
+        link: 'test2',
+        description: 'test2'
+    }
+]
+
+db.Book
+    .remove({})
+    .then(() => db.Book.collection.insertMany(bookSeed))
+    .then(data => {
+        console.log('Inserted records');
+        process.exit(0)
+    })
+    .catch(err => {
+        console.log(err);
+        process.exit(0)
+    })
